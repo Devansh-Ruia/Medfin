c2a7f17dbb964ad6796a41da2c983dd39683c7e2
Devansh Ruia <ruiadevansh@gmail.com>
2026-01-20 15:53:02 -0800

    Prepare for custom domain setup - update CORS and environment variables

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GHU35a8fHVVvoFiudzS/4eEiR7m/HaWiUEQBM+bMLnM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VqdJOhwLh9ocLCcmotvNkUuaF8yDIzn+j1fEOgGTHls=\"\n}"
